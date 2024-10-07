380b84827286b00b68bd993705dedb9d18de8782
JosueChin01 <gjosue898@gmail.com>
2024-10-06 23:48:18 -0700

    Add files via upload

---
+++ DSSEscoringBueno/Catalogos/MenuPrincipal.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace DSSEscoringBueno.Catalogos
+{
+    partial class MenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuPrincipal));
+            this.label1 = new System.Windows.Forms.Label();
+            this.ComBoxCatalogos = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione su espacio de trabajo:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ComBoxCatalogos
+            // 
+            this.ComBoxCatalogos.DropDownWidth = 202;
+            this.ComBoxCatalogos.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComBoxCatalogos.FormattingEnabled = true;
+            this.ComBoxCatalogos.Location = new System.Drawing.Point(91, 33);
+            this.ComBoxCatalogos.Name = "ComBoxCatalogos";
+            this.ComBoxCatalogos.Size = new System.Drawing.Size(257, 23);
+            this.ComBoxCatalogos.TabIndex = 1;
+            this.ComBoxCatalogos.SelectedIndexChanged += new System.EventHandler(this.ComBoxCatalogos_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(91, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(659, 331);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(16, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(354, 75);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Aplicacion DSS\r\nDesarrollado por el Gpo.01 de la carrera de Ingenieria de Softwar" +
+    "e \r\nUniversidad Autonoma de Occidente Campus Los Mochis\r\nAcargo del Profesor Edg" +
+    "ar Omar Pérez Contreras\r\n\r\n";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DSSEscoringBueno.Properties.Resources.UADEOLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(380, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ComBoxCatalogos);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MenuPrincipal";
+            this.Text = "MenuPrincipal";
+            this.Load += new System.EventHandler(this.MenuPrincipal_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ComBoxCatalogos;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ DSSEscoringBueno/Catalogos/Proyectos.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DSSEscoringBueno.Catalogos
+{
+    public partial class Proyectos : Form
+    {
+        string conexion = "Data Source=JOSUECHIN\\JOSUECHINBD;Initial Catalog=\"DB_DSS\";Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+        public Proyectos()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+        
+            // Conectar a la base de datos y ejecutar la consulta
+            using (SqlConnection connection = new SqlConnection(conexion))
+            {
+                try
+                {
+                    // Abrir la conexión
+                    connection.Open();
+
+                    // Consulta SQL para obtener el último registro basado en el ID más alto
+                    string query = "SELECT TOP 1 ID_Proy FROM Proyecto ORDER BY ID_Proy DESC";
+
+                    // Crear el comando SQL
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Ejecutar la consulta y obtener el resultado
+                    object result = command.ExecuteScalar();
+
+                    // Verificar si se obtuvo algún resultado
+                    if (result != null)
+                    {
+                        // Mostrar el nombre en textBox1
+                        txtId.Text = result.ToString();
+                    }
+                    else
+                    {
+                        // Si no se encontró ningún registro
+                        MessageBox.Show("No se encontró ningún registro.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Manejar cualquier error de conexión o consulta
+                    MessageBox.Show("Error al conectar a la base de datos: " + ex.Message);
+                }
+            }
+        }
+
+        private void Proyectos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //incompleto
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("insert into Proyecto(Nombre_Proy,Objetivo_Proy) values ('" + txtNombre + "','" + txtObjetivo + "')", cn);
+                cmd.CommandType = CommandType.Text;
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void textBoxId_TextChanged(object sender, EventArgs e)
+        {
+            
+            {
+                
+            }
+                
+        }
+
+        //abre el menu principal 
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Desea Cerrar la ventana anterior", "ADVERTENCIA", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Form menuPrincipal = new MenuPrincipal();
+                menuPrincipal.Show();
+                this.Hide();
+            }
+            else
+            {
+                Form menuPrincipal = new MenuPrincipal();
+                menuPrincipal.Show();
+            }
+        }
+
+        private void Limpiar()
+        {
+            txtId.Text = "0";
+            txtNombre.Text = "";
+            txtObjetivo.Text = "";
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+    }
+}
